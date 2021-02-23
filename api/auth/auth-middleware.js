f661e597ce1f053d24bbc9235d1265159c6e7839
Plofland <ploflan@gmail.com>
2021-02-23 15:56:35 -0500

    Built out users-router, users-model & added token auth to auth middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const {jwtSecret} = require('../../config/secrets')
+
 module.exports = {
   isValid,
   checkRole
@@ -14,5 +17,25 @@ const checkRole = (role) => (req, res, next) => {
     res
       .status(403)
       .json('You are not an admin, access restricted');
+  }
+}
+
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).json('No token');
+  } else {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        res
+          .status(401)
+          .json('The token is bad ' + err.message);
+      } else {
+        //this is how we will have access to the token info
+        req.decodedToken = decoded;
+        next();
+      }
+    });
   }
 }
---
+++ api/users/users-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
+const checkRole = require('../auth/auth-middleware')
+
+router.get("/", restricted, checkRole('admin'), (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+module.exports = router;
